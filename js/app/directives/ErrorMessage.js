fdbe4a535ab810af7e75facef1143adeb35571e8
Chen Yang <yangc5@mail.uc.edu>
2016-03-20 19:19:35 +0000

    done

---
+++ js/app/directives/ErrorMessage.js
@@ -0,0 +1,8 @@
+function ErrorMessage() {
+  return {
+    replace: true,
+    template: '<div>Oh no, an error occurred!</div>'
+  }
+}
+
+angular.module('app').directive('errorMessage', ErrorMessage);
