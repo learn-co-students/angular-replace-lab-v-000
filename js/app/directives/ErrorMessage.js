94fe831764d65876cbe79060eeb3c7241150e94f
selichter <lichter.sarahe@gmail.com>
2016-04-07 10:46:46 -0500

    Done.

---
+++ js/app/directives/ErrorMessage.js
@@ -0,0 +1,11 @@
+function ErrorMessage() {
+  return {
+    template: '<div>Oh no, an error occurred!</div>',
+    replace: true
+
+  };
+}
+
+angular
+  .module('app')
+  .directive('errorMessage', ErrorMessage);
---
+++ js/app/directives/SuccessMessage.js
@@ -0,0 +1,11 @@
+function SuccessMessage() {
+  return {
+    template: '<div>Woohoo! Success!</div>',
+    replace: true
+
+  };
+}
+
+angular
+  .module('app')
+  .directive('successMessage', SuccessMessage);
