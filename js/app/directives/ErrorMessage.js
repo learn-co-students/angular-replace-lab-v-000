73e32b6e65eab0e1b5b24668101c7e5d14a8fbf3
thebrianemory <the@brianemory.com>
2016-07-24 12:17:16 -0700

    Done.

---
+++ js/app/directives/ErrorMessage.js
@@ -0,0 +1,8 @@
+function ErrorMessage() {
+  return {
+    replace: false,
+    template: '<div>Oh no, an error occurred!</div>'
+  };
+}
+
+angular.module('app').directive('errorMessage', ErrorMessage);
---
+++ js/app/directives/SuccessMessage.js
@@ -0,0 +1,8 @@
+function SuccessMessage() {
+  return {
+    replace: true,
+    template: '<div>Woohoo! Success!</div>'
+  };
+}
+
+angular.module('app').directive('successMessage', SuccessMessage);
