6874af8e1265fb2a5325b7e65cd99f43441b25c3
Ziv Zamechek <ziv921@yahoo.com>
2016-08-19 10:15:02 -0400

    Done.

---
+++ js/app/directives/SuccessMessage.js
@@ -0,0 +1,11 @@
+
+function SuccessMessage() {
+    return {
+        replace: true,
+        template: '<div>Woohoo! Success!</div>'
+    };
+}
+ 
+angular
+    .module('app')
+    .directive('successMessage', SuccessMessage);
