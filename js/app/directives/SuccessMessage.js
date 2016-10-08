c359dcdf23190292b5f25bb17d1d2b73bfdff408
Chris-Sexton <champlain2013@gmail.com>
2016-10-08 13:04:16 -0400

    Done.

---
+++ js/app/directives/SuccessMessage.js
@@ -0,0 +1,9 @@
+function SuccessMessage(){
+  return {
+    replace: true,
+    templateUrl: 'js/app/views/success.html'
+  }
+}
+
+angular.module('app')
+    .directive('successMessage', SuccessMessage);
